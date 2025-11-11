64e7d91c3b836944bdccdc00450a7fb56cd07bcd
Angrini Necy Sirait <siraitangrini@gmail.com>
2025-11-11 06:20:20 -0800

    ANG
    SELESAI

--- TFUNC.java
+++ TFUNC.java
@@ -1,27 +1,27 @@
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n, hasil;
+        int n, result;
 
         n = Integer.parseInt(input.nextLine());
-        hasil = pertambahan(n);
+        result = angka(n);
         System.out.println("Bilangan yang dimasukkan : " + n);
-        System.out.println("Hasil perhitungan rekursif : " + hasil);
+        System.out.println("Hasil perhitungan rekursif : " + result);
     }
     
-    public static int pertambahan(int n) {
-        int hasil;
+    public static int angka(int n) {
+        int result;
 
         if (n == 1) {
-            hasil = 1;
+            result = 1;
         } else {
-            hasil = pertambahan(n - 1) + n;
+            result = angka(n - 1) + n;
         }
         
-        return hasil;
+        return result;
     }
 }

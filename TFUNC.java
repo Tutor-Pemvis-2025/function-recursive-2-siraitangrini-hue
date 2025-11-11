425143c44af17759d640516d2b809eefffaf0d26
Angrini Necy Sirait <siraitangrini@gmail.com>
2025-11-11 06:13:34 -0800

    Update TFUNC.java

---
+++ TFUNC.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, hasil;
+
+        n = Integer.parseInt(input.nextLine());
+        hasil = pertambahan(n);
+        System.out.println("Bilangan yang dimasukkan : " + n);
+        System.out.println("Hasil perhitungan rekursif : " + hasil);
+    }
+    
+    public static int pertambahan(int n) {
+        int hasil;
+
+        if (n == 1) {
+            hasil = 1;
+        } else {
+            hasil = pertambahan(n - 1) + n;
+        }
+        
+        return hasil;
+    }
+}
